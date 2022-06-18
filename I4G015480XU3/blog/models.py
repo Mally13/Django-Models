5fffb6cc99a1c18ebfce892e454f8316a13c3534
Mally13 <mallymutuku13@gmail.com>
2022-06-18 21:10:17 +0300

    post

--- I4G015480XU3/blog/models.py
+++ I4G015480XU3/blog/models.py
@@ -1,3 +1,11 @@
+from datetime import datetime
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Post(models.Model):
+    Title=models.CharField(max_length=200)
+    Text=models.TextField(any)
+    Author=models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    Created_date=models.DateTimeField(datetime)
+    Published_date=models.DateTimeField(datetime)
